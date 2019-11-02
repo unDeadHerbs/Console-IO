b862f7a258193d85713ec8e2d8e75bec5f901b6a
unDeadHerbs <unDeadHerbs@gmail.com>
2019-11-02 06:58:26 -0500

    updated .clangformat

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-#include "ncursespp.hpp"
 #include <unistd.h>
+#include "ncursespp.hpp"
 
 int main() {
-  sleep(1);
-  auto screen = ncurses::Get();
-  screen->refreshScreen();
-  sleep(1);
-  screen->getFrame()[1][1] = 'a';
-  screen->refreshScreen();
-  sleep(1);
+	sleep(1);
+	auto screen = ncurses::Get();
+	screen->refreshScreen();
+	sleep(1);
+	screen->getFrame()[1][1] = 'a';
+	screen->refreshScreen();
+	sleep(1);
 }
